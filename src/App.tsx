e816d24b753805f001044023321544543ebcc3cf
Steven <steven.liu@hivery.com>
2023-07-03 11:35:44 +1000

    update route

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
-import { BrowserRouter as Router, useRoutes } from "react-router-dom";
-import About from './components/pages/About';
+import {
+    BrowserRouter as Router,
+    Route,
+    Routes,
+} from "react-router-dom";
+import About from "./components/pages/About";
 
 import Header from "./components/Header";
 import Thoughts from "./components/pages/Thought";
 import Project from "./components/pages/Project";
 
 function App() {
-  const routes = useRoutes([
-    {path: "/", element: <About />},
-    {path: "/projects", element: <Project />},
-    {path: "/thoughts", element: <Thoughts />}
-  ])
-  return (
-    <div className="App">
-      <Header />
-      {routes}
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Routes>
+                <Route path="/" Component={About} />
+                <Route path="/projects" Component={Project} />
+                <Route path="/about" Component={Thoughts} />
+            </Routes>
+        </div>
+    );
 }
 
 const AppWrapper = () => {
-  return (
-    <Router>
-      <App />
-    </Router>  
-  )
-}
+    return (
+        <Router>
+            <App />
+        </Router>
+    );
+};
 
 export default AppWrapper;
